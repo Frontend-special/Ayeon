e6480d06cf4c86cc15c0b4753f3b1cd04457b4ac
joayeon <whffu0156@gmail.com>
2022-12-31 00:29:56 +0900

    Comment: 주석 추가

--- 과제/let.js
+++ 과제/let.js
@@ -1,11 +1,11 @@
 /*
 
 변수 위치 찾기
-변수 a는 main 함수 안에서만 사용된다.(지역변수)
-변수 b는 dom api selector로 다른 이벤트에서 사용될 여지가 있다.(전역변수..?)
-변수 c는 메인 함수 안에서 실행되는 콜백함수 solution의 인자이다.(지역...)
-변수 d는 솔루션 함수에서 전달받은 c를 다른 형태로 바꿔준다.(지..역?)
-변수 e는 main함수의 최종 반환 값으로 향후 다른 함수에서 재사용된다.(전역변수)
+변수 a는 main 함수 안에서만 사용된다.
+변수 b는 dom api selector로 다른 이벤트에서 사용될 여지가 있다.
+변수 c는 메인 함수 안에서 실행되는 콜백함수 solution의 인자이다.
+변수 d는 솔루션 함수에서 전달받은 c를 다른 형태로 바꿔준다.
+변수 e는 main함수의 최종 반환 값으로 향후 다른 함수에서 재사용된다.
 
 */
 
@@ -20,3 +20,20 @@ function main() {
     solution();
     let d;
 }
+
+/*
+let b;
+let e;
+function solution(c) {
+    let d;
+}
+function parse() {}
+function main() {
+    let a;
+    let c;
+    
+    solution(c);
+}
+
+e = main(solution);
+*/

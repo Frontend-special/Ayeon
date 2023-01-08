590e09b2d4fe2c843bb3638df46934f06d569526
joayeon <whffu0156@gmail.com>
2023-01-08 14:51:14 +0900

    Feat: 3번...

--- day02/level2/level2.js
+++ day02/level2/level2.js
@@ -7,21 +7,45 @@ console.log(RESERVATION_LIST);
 1. 요구사항 확인하기
     고객 이름과 연락처를 입력 받아 예약이 되어 있는지 확인하고 되어 있으면 예약번호 보여주기
 
-2. 요구사항 바탕으로 인풋 생각하기
-    (1) 고객 이름과 연락처를 적을 수 있는 input
-    (2) 이름과 연락처의 값을 받아서 넘겨줄 button
-    (3) 이름과 연락처를 받아서 예약이 되어 있는지 확인할 함수
-    (4) 확인한 후에 예약됐으면 예약번호를 출력해 줄 input 태그, 
-        예약이 되어 있지 않다면 alert 창으로 예약이 되어 있지 않다고 보여주기
-
 3. 인풋 바탕으로 단계별 한글로 함수 설계하기
-    (1) 이름과 연락처 값을 value로 받아오기
-    (2) 받아온 값을 비교하는 if문
-        if문을 사용해서 RESERVATION_LIST와 비교한 후에 맞으면 p 태그에 innerText 사용해서 예약 number 보여주기
-    (3) RESERVATION_LIST와 다를시 alert 창으로 예약이 되어 있지 않다고 보여주기
+    (1) RESERVATION_LIST에서 사용자가 입력한 이름과 같은 요소 찾기 => 배열에서 특정 값을 만족하는 배열 찾기
+    (2) 이름이 없을 경우에는 일치하지 않는다고 alert으로 알려주기
+    (3) 이름이 있을 경우 핸드폰 번호가 일치하는지 확인하기 => 배열에서 특정 값을 만족하는 요소 찾기 
+    (4) input 값이 입력되었을 때 자동으로 전화번호 하이픈
 
 4. 단계별 구글링 키워드 생각하기
-    (1) input 태그에 있는 값을 button 으로 받아오는 방법
-    (2) 객체와 값을 비교하기 위한 방법
-    (3) p 태그에 글자를 추가하는 방법 || alert 창을 띄울 수 있는 방법
 */
+
+const $userName = document.querySelector('[name="user-name"]');
+const $userPhone = document.querySelector('[name="user-phone"]');
+const $btn = document.querySelector('button');
+const $reservNum = document.querySelector('#reservation-number');
+
+console.log($userName);
+
+// 물어볼 부분 1
+$userPhone.addEventListener('input', (e) => {
+    $userPhone.value = e.target.value;
+    e.target.value = e.target.value
+        .replace(/[^0-9]/g, '')
+        .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, '$1-$2-$3')
+        .replace(/(\-{1,2})$/g, '');
+
+    return e.target.value;
+});
+
+$btn.addEventListener('click', () => {
+    const userNameValue = $userName.value;
+    const userPhoneValue = $userPhone.value;
+
+    const listName = RESERVATION_LIST.filter((el) => (el.name = userNameValue));
+    if (listName.length == 0) return alert('일치하는 이름이 없습니다.');
+
+    const listPhone = listName.find((list) => list.phone == userPhoneValue);
+    if (!listPhone) return alert('일치하는 번호가 없습니다.');
+
+    return ($reservNum.innerText = listPhone.number);
+});
+
+// form 이벤트
+// find / filter 반환값
--- day02/level3/level3.js
+++ day02/level3/level3.js
@@ -7,20 +7,39 @@
     만약 같은 재료를 입력하고 추가 버튼을 누르게 될 경우 '재료가 이미 있다'고 보여져야 한다.
 
 2. 요구사항 바탕으로 인풋 생각하기
-    (1) 재료명과 용량을 입력받을 input 태그
-    (2) 재료명과 용량의 값을 받아서 넘겨줄 button 태그
-    (3) 값을 받아서 화면에 보여줄 표(table)
-    (4) 표에 재료명과 용량, 관리에 표시해줄 함수가 필요
-    (5) 표에 표시된 내용을 제출할 수 있는 button 태그와 제출 후 목록을 보여줄 함수
-        만약 같은 재료가 제출됐다면 이미 있다고 보여줄 alert창
 
 3. 인풋 바탕으로 단계별 한글로 함수 설계하기
-    (1) input 태그에서 재료명과 용량의 값을 value로 받아오기
-    (2) 값을 받아서 표에 표시해줄 수 있는 함수
-    (3) 값이 중복인지 아닌지 확인할 수 있는 if문 및 중복이라면 alert창 띄우기
+    (1) 입력한 재료명과 용량의 값을 받아와서 표에 추가하기
+    (2) 표에 있는 재료를 제출했을 때 ingredient-list에 추가하기
+    (3) 표에 적힌 내용과 동일한 값이 입력되었을 때 이미 입력되어 있다고 alert으로 알려주기
 
 4. 단계별 구글링 키워드 생각하기
     (1) input 태그에서 값을 받아올 수 있는 방법
     (2) input 에서 받은 값을 js로 table태그에 보여줄 수 있는 법
     (3) 값이 중복인지를 체크하는 if문 || alert를 js로 띄우는 방법
 */
+
+const $ingredient = document.querySelector('[name="ingredient"]');
+const $weight = document.querySelector('[name="weight"]');
+const $addBtn = document.querySelector('.add-button');
+const $table = document.querySelector('table');
+
+console.log($ingredient);
+console.log($weight);
+console.log($addBtn);
+
+$addBtn.addEventListener('click', () => {
+    // 재료와 무게의 값을 받아옴
+    const ingredientValue = $ingredient.value;
+    const weightValue = $weight.value;
+
+    // 이미 같은 재료가 들어가 있는지 검사를 해줘야 됨
+    // tr 태그에 추가해서 받아온 값을 innerhtml로 추가해줘야 함
+    if()
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${ingredientValue}</td>
+    <td>${weightValue}</td>`;
+
+    // 추가가 된 것 같은데 몬가.. 안 됨..?..?
+    $table.append(tr);
+});
